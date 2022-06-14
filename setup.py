816f05f3bc5ced14f9c99439fd89fefb326b4da6
gripani <g.ripani93@gmail.com>
2022-06-14 14:34:18 +0200

    add README file

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from distutils.core import setup
 from sys import argv
 from utils import build_fortran 
+from pathlib import Path 
 
 if __name__ == '__main__':
 
     if argv[1] == 'build':
         build_fortran()
 
+    pwd = Path(__file__).parent 
+    long_description = (pwd / "README.md").read_text()
+
     setup(
         name='FortranPython', 
         version='0.0.1', 
         packages=['FortranPython'], 
         author='Giorgio Ripani', 
-        author_email='g.ripani93@gmail.com'
+        author_email='g.ripani93@gmail.com',
+        long_description=long_description,
     )
